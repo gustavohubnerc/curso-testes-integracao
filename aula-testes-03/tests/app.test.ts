73dc8ec4eb93da29da4e4ba1c8131c7503b3d436
gustavohubnerc <hubnergc@gmail.com>
2023-09-26 12:14:21 -0300

    aula 3

---
+++ aula-testes-03/tests/app.test.ts
@@ -0,0 +1,14 @@
+describe("calculator", () => {
+    it("should return the sum of two numbers", () => {
+        expect(1 + 2).toBe(3);
+    })
+    it("should return the sub of two numbers", () => {
+        expect(1 - 2).toBe(-1);
+    })
+    it("should return the mul of two numbers", () => {
+        expect(1 * 2).toBe(2);
+    })
+    it("should return the div of two numbers", () => {
+        expect(1 / 2).toBe(0.5);
+    })
+}) 
